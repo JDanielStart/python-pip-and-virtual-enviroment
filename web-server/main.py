c9091eafdf916594b6c9c21ed0a72a7840554a3e
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-04-12 01:58:39 +0100

    Add Fast api and create server with Fast api

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [1,2,3]
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_contact():
+    return '''
+    <h1>Hola esto es una prueba</h1>
+    <p>Esto es un parrafo</p>
+    '''
 
 def run():
     store.get_categories()
