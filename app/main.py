426412f6261178e1580a278ae3ce2d02b7ad3666
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-04-12 01:40:47 +0100

    Using Pandas

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  '''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'Europe',data))
 
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
+  '''
   
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
@@ -17,7 +27,7 @@ def run():
     country = result[0]
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country/Territory'], labels, values)
-    #charts.generate_pie_chart(country['Country/Territory'], countries, percentages)
+    charts.generate_pie_chart(country['Country/Territory'], countries, percentages)
 
 
 if __name__ == '__main__':
